400c9846dd944d3e488d2ea8ca21fd9abcbd6cb8
Kevin Zhang <hello@kevinzha.ng>
2020-06-03 03:38:38 -0400

    sampling code

--- globals/Math.gd
+++ globals/Math.gd
@@ -10,3 +10,12 @@ func _init():
 
 func roll(begin:int = 1, end:int = 6):
 	return prng.randi_range(begin, end)
+
+func sample(a:Array, n:int = 1):
+	var set = a.duplicate()
+	var sample = []
+	for i in range(n):
+		var idx = prng.randi() % set.size()
+		sample.append(set[idx])
+		set.remove(idx)
+	return sample
