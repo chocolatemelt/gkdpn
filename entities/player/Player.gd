9715d9cd65f5e5092ee2ace521e99273e7499947
Kevin Zhang <kzhang@hubspot.com>
2021-11-26 23:54:15 -0500

    isometric player movement

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -1,12 +1,29 @@
 extends KinematicBody2D
 
-const MOTION_SPEED = 200 # px/s
+const MOTION_SPEED = 400 # px/s
+
+var motion = Vector2()
+
+func cartesian_to_isometric(cartesian):
+	var screen_pos = Vector2()
+	screen_pos.x = cartesian.x - cartesian.y
+	screen_pos.y = (cartesian.x + cartesian.y) / 2
+	return screen_pos
 
 func _physics_process(_delta):
-	var motion = Vector2()
-	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	motion.y *= 0.5
-	motion = motion.normalized() * MOTION_SPEED
+	var direction = Vector2()
+	if Input.is_action_pressed("move_up"):
+		direction += Vector2(0, -1)
+	elif Input.is_action_pressed("move_down"):
+		direction += Vector2(0, 1)
+	
+	if Input.is_action_pressed("move_left"):
+		direction += Vector2(-1, 0)
+	elif Input.is_action_pressed("move_right"):
+		direction += Vector2(1, 0)
+
+	motion = direction.normalized() * MOTION_SPEED * _delta
+	motion = cartesian_to_isometric(motion)
+
 	#warning-ignore:return_value_discarded
 	move_and_slide(motion)
